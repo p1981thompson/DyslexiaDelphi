74321ccb4c2ffb2d72a841f3908b258e2fdd224d
p1981thompson <paul.thompson@psy.ox.ac.uk>
2020-04-14 17:31:00 +0100

    corrected mail script

--- reports/mail script terminology.R
+++ reports/mail script terminology.R
@@ -13,18 +13,10 @@ library(mailR)
 library(sendmailR)
 library(readxl)
 library(psych)
-################################################################
-
-qualtrics2.data<-read_excel("H:/DVMB/CREWS_delphi/Round2/test data2/CATALISE_2B.xlsx",sheet="CATALISE_2")
-
-
-################################################################
-
-
 
 #delphi.data<-read.xlsx(paste(getwd(),"\\delphi data\\delphi_results.xlsx",sep=""),sheetName="Sheet1")
 
-delphi.CREW.email.data<-read_excel("H:/DVMB/CREWS_delphi/test data/CATALISE_terminology.xlsx",sheet="CREWS_T")
+delphi.CREW.email.data<-read_excel("/Volumes/PSYHOME/PSYRES/pthompson/DVMB/CREWS_delphi/test data/CREWS_terminology.xlsx",sheet="CREWS_T")
 
 Delphi.CREW.name<-apply(delphi.CREW.email.data[,c("firstname","lastname")], 1, paste, collapse=" ")
 Delphi.members.CREW<-cbind(delphi.CREW.email.data[,1:5],Delphi.CREW.name)
@@ -33,16 +25,21 @@ for(i in 1:6){Delphi.members.CREW[,i]<-as.character(Delphi.members.CREW[,i])}
 #####send result email with report (CREWS)#####
 
 #for(i in 51:55)
-  for(i in c(1:2))
+  for(i in c(1:1))
 {
   mailControl=list(smtpServer="smtp.ox.ac.uk")
   
   #key part for attachments, put the body and the mime_part in a list for msg
   attachmentObject <- mime_part(x=paste(getwd(),paste("/Delphi_CREWS_report_",Delphi.members.CREW[i,1],sep=""),".pdf",sep=""),name=paste("Delphi_CREW_report_",Delphi.members.CREW[i,1],".pdf",sep=""))
   #
-  body = paste("Dear ", Delphi.members.CREW[i,6],", \n \n Many thanks for participating in CREWS Delphi study.
+  body = paste("Dear ", Delphi.members.CREW[i,6],", \n 
+\n Many thanks for participating in CREWS Delphi study.
 \n Enclosed is a report showing the summary findings together with an indication of where your responses fell in the distribution. 
-\n \nBest wishes,\n \nPaul Thompson\n \nOn behalf of\n \n Professor Trish Greenhalgh,
+\n \nBest wishes,
+\n 
+\nPaul Thompson
+\n 
+\nOn behalf of\n \n Professor Trish Greenhalgh,
              \nDept of Primary Care Health Sciences, University of Oxford, OX2 6GG.\ntel +44 (0)1865 289294",sep="")
   
   bodyWithAttachment <- list(body,attachmentObject)
