b7489600b43d889c68961136f28136d1c81ff99e
Paul Thompson <paulthompson@msdits-mbp.home>
2020-04-20 15:05:05 +0100

    Adapted scripts for round 2 (scripts plus '_r2')

--- report generating_T_script_r2.R
+++ report generating_T_script_r2.R
@@ -1,13 +1,13 @@
 #Load data from Qualtrics RECAP #
-
-#07-04-2020#
+# Round 2 #
+#20-04-2020#
 
 setwd('/Volumes/PSYHOME/PSYRES/pthompson/DVMB/CREWS_delphi/')
 library(readxl)
 library(psych)
 library(tools)
 
-qualtrics.Terminology.data<-read_excel("/Volumes/PSYHOME/PSYRES/pthompson/DVMB/CREWS_delphi/test data/CREWS_terminology.xlsx",sheet="CREWS_T")#,col_types='text')
+qualtrics.Terminology.data<-read_excel("/Volumes/PSYHOME/PSYRES/pthompson/DVMB/CREWS_delphi/test data/CREWS_terminology_r2.xlsx",sheet="CREWS_T")#,col_types='text')
 
 qualtrics.Terminology.data<-as.data.frame(qualtrics.Terminology.data)
 
@@ -18,8 +18,8 @@ Delphi.T.members<-qualtrics.Terminology.data$ExternalDataReference
 
 processInput_T_Anon <- function(panel.member) { 
   temp <<- environment()
-  Sweave(paste(getwd(),'/reports/delphi_CREWS_report_template_anonymous.Rnw',sep=""), output = paste(getwd(),'/reports/Delphi_RECAP_report_anonymous.tex', sep = ''))
-  texi2dvi(paste(getwd(),'/reports/Delphi_RECAP_report_anonymous.tex', sep = ''), pdf = TRUE, clean = TRUE,texinputs=paste(getwd(),'/reports/Delphi_RECAP_report_anonymous.tex', sep = ''))
+  Sweave(paste(getwd(),'/reports/delphi_CREWS_report_template_anonymous_r2.Rnw',sep=""), output = paste(getwd(),'/reports/Delphi_RECAP_report_anonymous_Round2.tex', sep = ''))
+  texi2dvi(paste(getwd(),'/reports/Delphi_RECAP_report_anonymous_Round2.tex', sep = ''), pdf = TRUE, clean = TRUE,texinputs=paste(getwd(),'/reports/Delphi_RECAP_report_anonymous_Round2.tex', sep = ''))
 }
 
 # processInput_T <- function(panel.member) { 
@@ -30,6 +30,6 @@ processInput_T_Anon <- function(panel.member) {
 
 
 for(panel.member in Delphi.T.members){
-  Sweave(paste(getwd(),'/reports/delphi_CREWS_report_template.Rnw',sep=""), output = paste(getwd(),'/reports/Delphi_RECAP_report_', panel.member, '.tex', sep = ''))
-  texi2dvi(paste(getwd(),'/reports/Delphi_RECAP_report_', panel.member, '.tex', sep = ''), pdf = TRUE, clean = TRUE,texinputs=paste(getwd(),'/reports/Delphi_RECAP_report_', panel.member, '.tex', sep = ''))
+  Sweave(paste(getwd(),'/reports/delphi_CREWS_report_template_r2.Rnw',sep=""), output = paste(getwd(),'/reports/Delphi_RECAP_report_', panel.member, '_Round2.tex', sep = ''))
+  texi2dvi(paste(getwd(),'/reports/Delphi_RECAP_report_', panel.member, '_Round2.tex', sep = ''), pdf = TRUE, clean = TRUE,texinputs=paste(getwd(),'/reports/Delphi_RECAP_report_', panel.member, '_Round2.tex', sep = ''))
 }
