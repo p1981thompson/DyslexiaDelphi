16e40b8f9b081f2d880acc55d6a7b2f18e2eb7ab
Paul Thompson <paulthompson@msdits-mbp.home>
2020-05-12 19:50:37 +0100

    adjustment to delphi script to omit responses with errors.

--- report generating_T_script_r4.R
+++ report generating_T_script_r4.R
@@ -33,3 +33,18 @@ for(panel.member in Delphi.T.members){
   Sweave(paste(getwd(),'/reports/delphi_CREWS_report_template_r4.Rnw',sep=""), output = paste(getwd(),'/reports/Delphi_RECAP_report_', panel.member, '_Round4.tex', sep = ''))
   texi2dvi(paste(getwd(),'/reports/Delphi_RECAP_report_', panel.member, '_Round4.tex', sep = ''), pdf = TRUE, clean = TRUE,texinputs=paste(getwd(),'/reports/Delphi_RECAP_report_', panel.member, '_Round4.tex', sep = ''))
 }
+
+
+##########################################################
+
+# After omitting following IDs: c(11582826121, 11576211166, 11574988498, 11564087723, 11571347031, 11564508478)
+
+qualtrics.Terminology.data<-read_excel("/Volumes/PSYHOME/PSYRES/pthompson/DVMB/CREWS_delphi/test data/CREWS_terminology_r4_omit_confused.xlsx",sheet="CREWS_T")#,col_types='text')
+
+qualtrics.Terminology.data<-as.data.frame(qualtrics.Terminology.data)
+Delphi.T.members<-qualtrics.Terminology.data$ExternalDataReference
+
+processInput_T_Anon <- function(panel.member) { 
+       temp <<- environment()
+       Sweave(paste(getwd(),'/reports/delphi_CREWS_report_template_anonymous_r4_omit.Rnw',sep=""), output = paste(getwd(),'/reports/Delphi_RECAP_report_anonymous_Round4_omit.tex', sep = ''))
+       texi2dvi(paste(getwd(),'/reports/Delphi_RECAP_report_anonymous_Round4_omit.tex', sep = ''), pdf = TRUE, clean = TRUE,texinputs=paste(getwd(),'/reports/Delphi_RECAP_report_anonymous_Round4_omit.tex', sep = ''))
